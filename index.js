735e2011f640eea49b269b23866062473eecee81
Vera Shneerson <vera.shneerson@gmail.com>
2016-01-20 02:49:56 +0000

    reading index response from file

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var express = require('express')
+var express = require('express'),
+    fs = require('fs')
+
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  #response.send('Hello World 2!')
+  var buf = fs.readFile('index.html', 'utf8')
+  response.send(buf.toString)
 })
 
 app.listen(app.get('port'), function() {
