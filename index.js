d4790b6e33a74b28fb5a58d4b7591e2988890d9d
Vera Shneerson <vera.shneerson@gmail.com>
2016-01-24 01:13:36 +0000

    add HTML content

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var fs = require('fs')
 
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+console.log("hello world")
+
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
